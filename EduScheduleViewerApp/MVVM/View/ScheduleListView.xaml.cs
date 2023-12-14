dc8b1e9976cf5c06cf2c1c09d3b76bf13cf0171e
Vlad Zelinskyi <zelinskyi.me@gmail.com>
2023-12-14 16:06:08 +0200

    Created Schedule List View and binded it to ViewModel Schedule

---
+++ EduScheduleViewerApp/MVVM/View/ScheduleListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EduScheduleViewerApp.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class ScheduleListView : UserControl
+    {
+        public ScheduleListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EduScheduleViewerApp/MVVM/ViewModel/ScheduleListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using EduScheduleViewerApp.Core;
+using EduScheduleViewerApp.MVVM.Model;
+using System.Collections.ObjectModel;
+
+namespace EduScheduleViewerApp.MVVM.ViewModel
+{
+    class ScheduleListViewModel
+    {
+        public ObservableCollection<ScheduleRow> Schedule { get; set; }
+
+        public ScheduleListViewModel()
+        {
+            Schedule = new ObservableCollection<ScheduleRow>();
+
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+        }
+    }
+}
