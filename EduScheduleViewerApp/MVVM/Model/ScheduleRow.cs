89a10ffdba17227c776c919b63e13822ffa0ad99
Vlad Zelinskyi <zelinskyi.me@gmail.com>
2023-12-14 13:33:46 +0200

    Created test model for schedule row
    Test model class for binding between Main View and ViewModel

---
+++ EduScheduleViewerApp/MVVM/Model/ScheduleRow.cs
@@ -0,0 +1,16 @@
+﻿namespace EduScheduleViewerApp.MVVM.Model
+{
+    class ScheduleRow
+    {
+        string Title { get; set; }
+        string StartTime { get; set; }
+        string EndTime { get; set; }
+
+        public ScheduleRow( string title, string startTime, string endTime)
+        {
+            Title = title;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    }
+}
