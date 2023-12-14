cc03748389c2e705d283210e5b78d6089acdff36
Vlad Zelinskyi <zelinskyi.me@gmail.com>
2023-12-14 16:07:24 +0200

    Added ContentControl to Main Window

---
+++ EduScheduleViewerApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using EduScheduleViewerApp.Core;
+
+namespace EduScheduleViewerApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ScheduleListViewModel ScheduleListVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            ScheduleListVM = new ScheduleListViewModel();
+
+            CurrentView = ScheduleListVM;
+        }
+
+    }
+}
