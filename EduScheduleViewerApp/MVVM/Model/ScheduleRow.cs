6f70649627cb75939e75633bdb5fda382a9c27c3
Vlad Zelinskyi <zelinskyi.me@gmail.com>
2023-12-15 21:38:55 +0200

    Wrote styles for Main View Schedule DataGrid

--- EduScheduleViewerApp/MVVM/Model/ScheduleRow.cs
+++ EduScheduleViewerApp/MVVM/Model/ScheduleRow.cs
@@ -5,12 +5,20 @@
         public string Title { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
+        public bool IsActive { get; set; } = false;
 
         public ScheduleRow( string title, string startTime, string endTime)
         {
             Title = title;
             StartTime = startTime;
             EndTime = endTime;
+        }
+        public ScheduleRow(string title, string startTime, string endTime, bool isActive)
+        {
+            Title = title;
+            StartTime = startTime;
+            EndTime = endTime;
+            IsActive = isActive;
         }
     }
 }
--- EduScheduleViewerApp/MVVM/ViewModel/ScheduleListViewModel.cs
+++ EduScheduleViewerApp/MVVM/ViewModel/ScheduleListViewModel.cs
@@ -12,14 +12,14 @@ namespace EduScheduleViewerApp.MVVM.ViewModel
         {
             Schedule = new ObservableCollection<ScheduleRow>();
 
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
-            Schedule.Add(new ScheduleRow("Урок 1", "8:00", "8:45"));
+            Schedule.Add(new ScheduleRow("Урок 1", "08:00", "08:45"));
+            Schedule.Add(new ScheduleRow("Урок 2", "08:55", "09:40"));
+            Schedule.Add(new ScheduleRow("Урок 3", "09:50", "10:35"));
+            Schedule.Add(new ScheduleRow("Урок 4", "10:45", "11:30", true));
+            Schedule.Add(new ScheduleRow("Урок 5", "11:40", "12:25"));
+            Schedule.Add(new ScheduleRow("Урок 6", "12:35", "12:20"));
+            Schedule.Add(new ScheduleRow("Урок 7", "12:30", "13:15"));
+            Schedule.Add(new ScheduleRow("Урок 8", "14:25", "15:10"));
         }
     }
 }
