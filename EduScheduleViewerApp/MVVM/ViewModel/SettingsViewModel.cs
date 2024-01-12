bd4c57c5c3558d1a5b5697723497313fba0299cf
Vlad Zelinskyi <zelinskyi.me@gmail.com>
2024-01-12 22:05:59 +0200

    Styles for Schedule Settings Tab Item Headers

--- EduScheduleViewerApp/MVVM/ViewModel/SettingsViewModel.cs
+++ EduScheduleViewerApp/MVVM/ViewModel/SettingsViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace EduScheduleViewerApp.MVVM.ViewModel
+﻿using EduScheduleViewerApp.Core;
+using EduScheduleViewerApp.MVVM.ViewModel.SettingsPagesViewModel;
+
+namespace EduScheduleViewerApp.MVVM.ViewModel
 {
-    class SettingsViewModel
+    class SettingsViewModel : ObservableObject
     {
+        public ScheduleSettingsViewModel ScheduleSettingsVM { get; set; }
+
+        private object _currentSettingsTab;
+
+        public object CurrentSettingsTab
+        {
+            get { return _currentSettingsTab; }
+            set
+            {
+                _currentSettingsTab = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SettingsViewModel()
+        {
+            ScheduleSettingsVM = new ScheduleSettingsViewModel();
+            CurrentSettingsTab = ScheduleSettingsVM;
+        }
     }
 }
